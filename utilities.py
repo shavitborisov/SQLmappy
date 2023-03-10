3579beeabba0909c2202f5e29ac700761b7667a3
shavitborisov <shavitborisov@gmail.com>
2023-03-10 12:13:22 +0200

    Added logging decorator

---
+++ utilities.py
@@ -0,0 +1,16 @@
+import logging
+
+
+def log(func: any) -> any:
+    """
+    Logging decorator to track executed function. Used for debugging.
+    @param func: The decorated function.
+    @return: Function func's result.
+    """
+
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        logging.debug("Executed " + func.__name__)
+        return result
+
+    return wrapper
